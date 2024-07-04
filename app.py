51f0190617d78306c981d9a693623c2a2a4102bb
chanablum1 <c0583275113@gmail.com>
2024-07-05 00:16:42 +0300

    added add car and urgent cars

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # from servise.add import add_contacts
 
@@ -8,16 +8,25 @@ car1 = {
     "id": "1",
     "number": "235-55-987",
     "problems": [],
-    "image": "https://img-optimize.toyota-europe.com/resize/ccis/680x680/zip/il/configurationtype/visual-for-grade-selector/product-token/aa41e8ae-82b4-4f15-8172-3bc596cc7762/grade/05fcd071-f774-48cd-9059-ac659904e114/body/74469257-47ae-46eb-859d-2c693e6726ca/fallback/true/padding/50,50,50,50/image-quality/70/day-exterior-4.png",
+    "urgent": True,
+    "image": "https://cdn.globalso.com/forthingmotor/Dongfeng-Forthing-Electric-Suv-Thunder-Ev-Sales-in-Europe4.jpg",
 }
 car2 = {
     "id": "2",
-    "number": "235-55-222",
+    "number": "545-55-222",
     "problems": [],
+    "urgent": True,
     "image": "https://t4.ftcdn.net/jpg/04/36/90/21/360_F_436902182_MyIDnh1EwT4MwN7UStL0IIpL5rO8Zsam.jpg",
 }
+car3 = {
+    "id": "3",
+    "number": "625-55-587",
+    "problems": [],
+    "urgent": False,
+    "image": "https://mg-israel.co.il/wp-content/uploads/2019/09/MG_ZS_EV_red.jpg",
+}
 
-cars = [car1, car2]
+cars = [car1, car2, car3]
 
 
 @app.route("/")
@@ -47,7 +56,22 @@ def single_car(id):
 @app.route("/add_car/")
 def add_car():
     print("****** Adding car")
-    return "Adding car"
+    return render_template("add_car.html")
+
+
+@app.route("/add_to_list/", methods=["POST", "GET"])
+def add_to_list():
+    print("****** Adding to list", request.form["cNumber"])
+    return "Added to list:" + request.form["cNumber"]
+
+
+@app.route("/urgent_cars/")
+def urgent_cars():
+    print("****** I am in urgent cars function")
+    urgent_cars_list = [car for car in cars if car["urgent"]]
+    print(f"****** Urgent cars list: {urgent_cars_list}")
+    return render_template("urgent_cars.html", cars=urgent_cars_list)
+
 
 
 if __name__ == "__main__":
